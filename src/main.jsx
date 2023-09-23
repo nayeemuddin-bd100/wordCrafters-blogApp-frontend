6f58b48ebb060821cb060b7b78d432a8a1af6fe7
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-09-24 02:59:57 +0600

    chor: redux toolkit provider added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
+import store from './redux/store/store';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);

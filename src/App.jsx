374cb967137e6205eba44084dd0a0fc880fb1c5b
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-09-29 02:59:26 +0600

    feat: category component added

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Navbar from "./components/Home/Navigation/Navbar";
+import AddNewCategory from './components/Category/AddNewCategory';
 
 
 function App() {
 	return (
 		<BrowserRouter>
-		<Navbar/>
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/home" element={<Home />} />
 				<Route path="/register" element={<Register />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/add-category" element={<AddNewCategory />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>

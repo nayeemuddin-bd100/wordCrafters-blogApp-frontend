99f14c90b137171933aa6d45973cc268567c0c1a
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-10-13 12:16:02 +0600

    feat: implement update,delete slice

--- src/redux/slices/category/categorySlices.js
+++ src/redux/slices/category/categorySlices.js
@@ -1,7 +1,10 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, createAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import baseUrl from "./../../../utils/baseUrl";
 
+
+
+// Add Category
 export const addCategoryAction = createAsyncThunk(
 	"category/add",
 	async (category, { rejectWithValue, getState }) => {
@@ -29,6 +32,7 @@ export const addCategoryAction = createAsyncThunk(
 	}
 );
 
+// Fetch All Category
 export const fetchCategoriesAction = createAsyncThunk(
 	"category/category-list",
 	async (category, { rejectWithValue, getState }) => {
@@ -56,12 +60,95 @@ export const fetchCategoriesAction = createAsyncThunk(
 	}
 );
 
+
+// Fetch Single Category
+export const fetchSingleCategoriesAction = createAsyncThunk(
+	"category/fetch-single-category",
+	async (id, { rejectWithValue, getState }) => {
+		
+		try {
+		
+			const state = getState();
+			const jwtToken = state?.users?.userAuth?.token;
+
+			const res = await axios.get(
+				`${baseUrl}/api/category/${id}`,
+				{
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${jwtToken}`,
+					},
+				}
+			);
+
+			return res.data;
+		} catch (error) {
+			if (!error?.response) {
+				throw error;
+			}
+			return rejectWithValue(error?.response?.data);
+		}
+	}
+);
+
+// update Category
+export const updateCategoriesAction = createAsyncThunk(
+	"category/update-category",
+	async (category, { rejectWithValue, getState,dispatch }) => {
+		try {
+			const state = getState();
+			const jwtToken = state?.users?.userAuth?.token;
+
+			const res = await axios.put(`${baseUrl}/api/category/${category?.id}`,
+				{title:category?.title},
+				{
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${jwtToken}`,
+				},
+			});
+
+			return res.data;
+		} catch (error) {
+			if (!error?.response) {
+				throw error;
+			}
+			return rejectWithValue(error?.response?.data);
+		}
+	}
+);
+
+// Delete Category
+export const deleteCategoriesAction = createAsyncThunk(
+	"category/delete-category",
+	async (id, { rejectWithValue, getState }) => {
+		try {
+			const state = getState();
+			const jwtToken = state?.users?.userAuth?.token;
+
+			const res = await axios.delete(`${baseUrl}/api/category/${id}`, {
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${jwtToken}`,
+				},
+			});
+
+			return res.data;
+		} catch (error) {
+			if (!error?.response) {
+				throw error;
+			}
+			return rejectWithValue(error?.response?.data);
+		}
+	}
+);
+
 const categorySlices = createSlice({
 	name: "category",
 	initialState: {},
-  extraReducers: (builder) => {
 
-		// add category
+
+  extraReducers: (builder) => {
     builder.addCase(addCategoryAction.pending, (state, action) => {
       state.loading = true;
       state.appErr = undefined;
@@ -83,12 +170,12 @@ const categorySlices = createSlice({
 
 		})
 
-
 		// category list
     builder.addCase(fetchCategoriesAction.pending, (state, action) => {
       state.loading = true;
       state.appErr = undefined;
       state.serverErr = undefined;
+			
 
     })
 
@@ -96,7 +183,11 @@ const categorySlices = createSlice({
       state.loading = false;
       state.categoryList = action?.payload;
       state.appErr = undefined;
-      state.serverErr = undefined;
+			state.serverErr = undefined;
+
+			// reset
+			state.updatedCategory = undefined;
+			state.category = undefined;
     })
 
     builder.addCase(fetchCategoriesAction.rejected, (state, action) => {
@@ -104,6 +195,76 @@ const categorySlices = createSlice({
       state.appErr = action?.payload?.message;
       state.serverErr = action?.error?.message;
 
+		})
+
+		// Fetch single Category
+    builder.addCase(fetchSingleCategoriesAction.pending, (state, action) => {
+      state.loading = true;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+
+    })
+
+    builder.addCase(fetchSingleCategoriesAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.categoryDetails = action?.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    })
+
+    builder.addCase(fetchSingleCategoriesAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+
+		})
+
+		
+		// Update Category
+    builder.addCase(updateCategoriesAction.pending, (state, action) => {
+      state.loading = true;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+
+    })
+
+    builder.addCase(updateCategoriesAction.fulfilled, (state, action) => {
+      state.loading = false;
+      state.updatedCategory = action?.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    })
+
+    builder.addCase(updateCategoriesAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+
+		})
+
+
+		// delete category
+    builder.addCase(deleteCategoriesAction.pending, (state, action) => {
+      state.loading = true;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+
+    })
+
+    builder.addCase(deleteCategoriesAction.fulfilled, (state, action) => {
+			const deletedCategory = action?.payload?._id
+		
+      state.loading = false;
+      state.categoryList = state.categoryList.filter(category => category._id !== deletedCategory)
+			state.updatedCategory = deletedCategory;
+      state.appErr = undefined;
+			state.serverErr = undefined;
+    })
+
+    builder.addCase(deleteCategoriesAction.rejected, (state, action) => {
+      state.loading = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
 		})
 
   }

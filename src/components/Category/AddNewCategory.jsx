704c12d3ad4ff9a75fd34afa58273466903f0c49
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-10-13 12:13:16 +0600

    feat:set router to navigate

--- src/components/Category/AddNewCategory.jsx
+++ src/components/Category/AddNewCategory.jsx
@@ -4,16 +4,11 @@ import { addCategoryAction } from './../../redux/slices/category/categorySlices'
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useSelector } from 'react-redux';
+import { Navigate } from "react-router-dom";
 
 const formSchema = Yup.object({
   title: Yup.string().required("Title is required"),
 })
-
-
-
-
-
-
 
 const AddNewCategory = () => {
  
@@ -28,101 +23,94 @@ const AddNewCategory = () => {
 		initialValues: {
 			title: "",
 		},
-		onSubmit: (value, { resetForm }) => {
+		onSubmit: (value) => {
 			dispatch(addCategoryAction(value));
-
-			resetForm({
-				initialValues: {
-					title: "",
-				},
-			});
 		},
 		validationSchema: formSchema,
 	});
 
-  
+	if (storeData.category) {
+		return <Navigate to="/category-list" />;
+	}
+		return (
+			<div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+				<div className="max-w-md w-full space-y-8">
+					<div>
+						<BookOpenIcon className="mx-auto h-12 w-auto" />
+						<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+							Add New Category
+						</h2>
+						<div className="mt-2 text-center text-sm text-gray-600">
+							<p className="font-medium text-indigo-600 hover:text-indigo-500">
+								These are the categories user will select when creating a post
+							</p>
 
-  return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-			<div className="max-w-md w-full space-y-8">
-				<div>
-					<BookOpenIcon className="mx-auto h-12 w-auto" />
-					<h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-						Add New Category
-					</h2>
-					<div className="mt-2 text-center text-sm text-gray-600">
-						<p className="font-medium text-indigo-600 hover:text-indigo-500">
-							These are the categories user will select when creating a post
-            </p>
-     
-            {
-              appErr || serverErr ?
-                <h2 className='text-red-500 text-center'>{ appErr } </h2> : null
-            }
+							{appErr || serverErr ? (
+								<h2 className="text-red-500 text-center">{appErr} </h2>
+							) : null}
+						</div>
 					</div>
-				</div>
-				{/* Form */}
-				<form onSubmit={formik.handleSubmit} className="mt-8 space-y-6">
-					<input type="hidden" name="remember" defaultValue="true" />
-					<div className="rounded-md shadow-sm -space-y-px">
-						<div>
-							<label htmlFor="email-address" className="sr-only">
-								Name
-							</label>
-							{/* Title */}
-							<input
-								onChange={formik.handleChange("title")}
-								onBlur={formik.handleBlur("title")}
-								type="text"
-								autoComplete="text"
-								value={formik.values.title}
-								className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-center focus:z-10 sm:text-sm"
-								placeholder="New Category"
-							/>
-							<div className="text-red-400 mb-2">
-								{formik.touched.title && formik.errors.title}
+					{/* Form */}
+					<form onSubmit={formik.handleSubmit} className="mt-8 space-y-6">
+						<input type="hidden" name="remember" defaultValue="true" />
+						<div className="rounded-md shadow-sm -space-y-px">
+							<div>
+								<label htmlFor="email-address" className="sr-only">
+									Name
+								</label>
+								{/* Title */}
+								<input
+									onChange={formik.handleChange("title")}
+									onBlur={formik.handleBlur("title")}
+									type="text"
+									autoComplete="text"
+									value={formik.values.title}
+									className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-center focus:z-10 sm:text-sm"
+									placeholder="New Category"
+								/>
+								<div className="text-red-400 mb-2">
+									{formik.touched.title && formik.errors.title}
+								</div>
 							</div>
 						</div>
-					</div>
 
-					<div>
 						<div>
-							{/* Submit */}
+							<div>
+								{/* Submit */}
 
-							{loading ? (
-                <button
-                  
-									type="submit"
-									className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-								>
-									<span className="absolute left-0 inset-y-0 flex items-center pl-3">
-										<PlusCircleIcon
-											className="h-5 w-5 text-yellow-500 group-hover:text-indigo-400"
-											aria-hidden="true"
-										/>
-									</span>
-									Loading ...
-								</button>
-							) : (
-								<button
-									type="submit"
-									className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-								>
-									<span className="absolute left-0 inset-y-0 flex items-center pl-3">
-										<PlusCircleIcon
-											className="h-5 w-5 text-yellow-500 group-hover:text-indigo-400"
-											aria-hidden="true"
-										/>
-									</span>
-									Add new Category
-								</button>
-							)}
+								{loading ? (
+									<button
+										type="submit"
+										className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										<span className="absolute left-0 inset-y-0 flex items-center pl-3">
+											<PlusCircleIcon
+												className="h-5 w-5 text-yellow-500 group-hover:text-indigo-400"
+												aria-hidden="true"
+											/>
+										</span>
+										Loading ...
+									</button>
+								) : (
+									<button
+										type="submit"
+										className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+									>
+										<span className="absolute left-0 inset-y-0 flex items-center pl-3">
+											<PlusCircleIcon
+												className="h-5 w-5 text-yellow-500 group-hover:text-indigo-400"
+												aria-hidden="true"
+											/>
+										</span>
+										Add new Category
+									</button>
+								)}
+							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				</div>
 			</div>
-		</div>
-	);
+		);
 };
 
 export default AddNewCategory;

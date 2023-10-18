9091a5343145a7138c8a775cf4d6c7a3c41c39c6
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-10-18 07:43:30 +0600

    feat: integrate all post api

--- src/components/Posts/PostsList.jsx
+++ src/components/Posts/PostsList.jsx
@@ -3,14 +3,32 @@ import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchAllPostsAction } from "../../redux/slices/posts/postSlices";
+
+import dateFormatter from "./../../utils/dateFormatter";
+import { fetchCategoriesAction } from "../../redux/slices/category/categorySlices";
+import MiniSpinner from "./../../utils/MiniSpinner";
+import { Spinner } from "./../../utils/Spinner";
+
 const PostsList = () => {
 	const dispatch = useDispatch();
 	const posts = useSelector((state) => state?.posts);
+	const { loading, postsList } = posts;
+
 	useEffect(() => {
 		dispatch(fetchAllPostsAction());
 	}, [dispatch]);
 
-	const { loading, postsList } = posts;
+	useEffect(() => {
+		dispatch(fetchCategoriesAction());
+	}, [dispatch]);
+
+	const category = useSelector((state) => state.category);
+	const {
+		categoryList,
+		loading: categoryLoading,
+		appErr: categoryErr,
+	} = category;
+
 	return (
 		<>
 			<section>
@@ -38,28 +56,35 @@ const PostsList = () => {
 									<h4 className="mb-4 text-gray-500 font-bold uppercase">
 										Categories
 									</h4>
-									<ul>
-										<div>Loading</div>
 
-										<div className="text-red-400 text-base">
-											Categories Error goes here
-										</div>
-
-										<div className="text-xl text-gray-100 text-center">
-											No category
-										</div>
-
-										<li>
-											<p className="block cursor-pointer py-2 px-3 mb-4 rounded text-yellow-500 font-bold bg-gray-500">
-												{/* {category?.title} */} category List
-											</p>
-										</li>
+									<ul className="flex flex-wrap place-content-evenly lg:block  ">
+										{categoryLoading ? (
+											<MiniSpinner />
+										) : categoryErr ? (
+											<div className="text-red-400 text-base">
+												{categoryErr}
+											</div>
+										) : categoryList?.length <= 0 ? (
+											<div className="text-xl text-gray-100 text-center">
+												No category
+											</div>
+										) : (
+											categoryList?.map((category) => {
+												return (
+													<li key={category._id}>
+														<p className="block cursor-pointer py-2 px-3 mb-4 rounded text-yellow-500 font-bold bg-gray-500">
+															{category?.title}
+														</p>
+													</li>
+												);
+											})
+										)}
 									</ul>
 								</div>
 							</div>
 							<div className="w-full lg:w-3/4 px-3">
 								{loading ? (
-									<h2 className="text-white text-2xl">Loading...</h2>
+									<Spinner />
 								) : postsList.length > 0 ? (
 									postsList.map((post) => {
 										return (
@@ -67,28 +92,28 @@ const PostsList = () => {
 												key={post._id}
 												className="flex flex-wrap bg-gray-900 -mx-3 my-5  lg:mb-6"
 											>
-												<div className="w-full lg:w-1/3 px-3">
-													<Link>
+												<div className="w-full md:w-1/3 px-3">
+													<Link className="flex justify-center md:block">
 														<img
-															className="w-full h-full object-cover rounded"
-															src="https://cdn.pixabay.com/photo/2021/02/24/23/43/boy-6047786_960_720.jpg"
+															className="w-52 object-fill rounded"
+															src={post?.image}
 															alt=""
 														/>
 													</Link>
 												</div>
-												<div className="w-full lg:w-2/3 px-3">
+												<div className="w-full md:w-2/3 px-3 flex justify-center items-center flex-col md:block">
 													<Link className="hover:underline">
 														<h3 className="mb-2 text-2xl text-green-400 font-bold font-heading">
-															{/* {capitalizeWord(post?.title)} */} post title
+															{post?.title}
 														</h3>
 													</Link>
-													<p className="text-gray-300">post description</p>
+													<p className="text-gray-300">{post?.description}</p>
 													<div className="mt-4 flex items-center">
 														<div className="flex-shrink-0">
 															<Link>
 																<img
 																	className="h-10 w-10 rounded-full"
-																	src="https://cdn.pixabay.com/photo/2021/02/24/23/43/boy-6047786_960_720.jpg"
+																	src={post?.author?.profilePhoto}
 																	alt=""
 																/>
 															</Link>
@@ -96,14 +121,12 @@ const PostsList = () => {
 														<div className="ml-3">
 															<p className="text-sm font-medium text-gray-900">
 																<Link className="text-yellow-400 hover:underline">
-																	user full name
+																	{post?.author?.firstName}{" "}
+																	{post?.author?.lastName}
 																</Link>
 															</p>
 															<div className="flex space-x-1 text-sm text-green-500">
-																<time>
-																	{/* <DateFormatter date={post?.createdAt} /> */}
-																	Post date
-																</time>
+																<time>{dateFormatter(post?.createdAt)}</time>
 																<span aria-hidden="true">&middot;</span>
 															</div>
 														</div>
@@ -111,16 +134,21 @@ const PostsList = () => {
 													<div className="mt-4 flex space-x-4">
 														<div className="flex items-center">
 															<ThumbUpIcon className="h-7 w-7 text-indigo-600 cursor-pointer" />
-															<span className="pl-2 text-gray-600">(2)</span>
+															<span className="pl-2 text-gray-600">
+																{" "}
+																{post?.likes?.length}{" "}
+															</span>
 														</div>
 														<div className="flex items-center">
 															<ThumbDownIcon className="h-7 w-7 cursor-pointer text-gray-600" />
-															<span className="pl-2 text-gray-600">(2)</span>
+															<span className="pl-2 text-gray-600">
+																{post?.disLikes?.length}
+															</span>
 														</div>
 														<div className="flex items-center">
 															<EyeIcon className="h-7 w-7 text-gray-400" />
 															<span className="pl-2 text-gray-600">
-																{/* {post?.numViews} */}2
+																{post?.numViews}
 															</span>
 														</div>
 													</div>
@@ -164,89 +192,3 @@ const PostsList = () => {
 };
 
 export default PostsList;
-
-
-	// <div
-	// 											key={post._id}
-	// 											className="flex flex-wrap bg-gray-900 -mx-3  lg:mb-6"
-	// 										>
-	// 											<div className="mb-10  w-full lg:w-1/4 px-3">
-	// 												<Link>
-	// 													{/* Post image */}
-	// 													<img
-	// 														className="w-full h-full object-cover rounded"
-	// 														src="https://cdn.pixabay.com/photo/2021/02/24/23/43/boy-6047786_960_720.jpg"
-	// 														alt=""
-	// 													/>
-	// 												</Link>
-	// 												{/* Likes, views dislikes */}
-	// 												<div className="flex flex-row bg-gray-300 justify-center w-full  items-center ">
-	// 													{/* Likes */}
-	// 													<div className="flex flex-row justify-center items-center ml-4 mr-4 pb-2 pt-1">
-	// 														{/* Togle like  */}
-	// 														<div className="">
-	// 															<ThumbUpIcon className="h-7 w-7 text-indigo-600 cursor-pointer" />
-	// 														</div>
-	// 														<div className="pl-2 text-gray-600">(2)</div>
-	// 													</div>
-	// 													{/* Dislike */}
-	// 													<div className="flex flex-row  justify-center items-center ml-4 mr-4 pb-2 pt-1">
-	// 														<div>
-	// 															<ThumbDownIcon className="h-7 w-7 cursor-pointer text-gray-600" />
-	// 														</div>
-	// 														<div className="pl-2 text-gray-600">(2)</div>
-	// 													</div>
-	// 													{/* Views */}
-	// 													<div className="flex flex-row justify-center items-center ml-4 mr-4 pb-2 pt-1">
-	// 														<div>
-	// 															<EyeIcon className="h-7 w-7  text-gray-400" />
-	// 														</div>
-	// 														<div className="pl-2 text-gray-600">
-	// 															{/* {post?.numViews} */}2
-	// 														</div>
-	// 													</div>
-	// 												</div>
-	// 											</div>
-	// 											<div className="w-full lg:w-3/4 px-3">
-	// 												<Link className="hover:underline">
-	// 													<h3 className="mb-1 text-2xl text-green-400 font-bold font-heading">
-	// 														{/* {capitalizeWord(post?.title)} */} post title
-	// 													</h3>
-	// 												</Link>
-	// 												<p className="text-gray-300">post description</p>
-	// 												{/* Read more */}
-	// 												<Link className="text-indigo-500 hover:underline">
-	// 													Read More..
-	// 												</Link>
-	// 												{/* User Avatar */}
-	// 												<div className="mt-6 flex items-center">
-	// 													<div className="flex-shrink-0">
-	// 														<Link>
-	// 															<img
-	// 																className="h-10 w-10 rounded-full"
-	// 																src="https://cdn.pixabay.com/photo/2021/02/24/23/43/boy-6047786_960_720.jpg"
-	// 																alt=""
-	// 															/>
-	// 														</Link>
-	// 													</div>
-	// 													<div className="ml-3">
-	// 														<p className="text-sm font-medium text-gray-900">
-	// 															<Link className="text-yellow-400 hover:underline ">
-	// 																user full name
-	// 															</Link>
-	// 														</p>
-	// 														<div className="flex space-x-1 text-sm text-green-500">
-	// 															<time>
-	// 																{/* <DateFormatter date={post?.createdAt} /> */}
-	// 																Post date
-	// 															</time>
-	// 															<span aria-hidden="true">&middot;</span>
-	// 														</div>
-	// 													</div>
-	// 												</div>
-	// 												{/* <p className="text-gray-500">
-  //     Quisque id sagittis turpis. Nulla sollicitudin rutrum
-  //     eros eu dictum...
-  //   </p> */}
-	// 											</div>
-	// 										</div>

4021dec5b8946efa159eb439ccb64ae5a1f409f2
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-11-11 03:11:28 +0600

    fix: resolve dateformatter function

--- src/utils/dateFormatter.js
+++ src/utils/dateFormatter.js
@@ -1,7 +1,13 @@
-import { format, parseISO } from "date-fns";
 
- const dateFormatter = (date) => {
-  return format(parseISO(date), "dd MMM yyyy");
-}
+ const dateFormatter = (inputDate) => {
+		const date = new Date(inputDate);
+
+		const day = date.getDate();
+		const month = date.toLocaleString("default", { month: "short" });
+		const year = date.getFullYear();
+
+		const formattedDate = `${day} ${month} ${year}`;
+		return formattedDate;
+ };
 
 export default dateFormatter

5c4b11009c48405d82c904f266a63184582d26f6
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-11-21 12:54:06 +0600

    feat: blocked user cannot comment

--- src/components/Comments/AddComment.jsx
+++ src/components/Comments/AddComment.jsx
@@ -12,6 +12,7 @@ const formSchema = Yup.object({
 	description: Yup.string().required("Description is required"),
 });
 
+
 const AddComment = ({ postId }) => {
 	const dispatch = useDispatch();
 
@@ -31,34 +32,41 @@ const AddComment = ({ postId }) => {
 		validationSchema: formSchema,
   });
   
-  
+	const posts = useSelector((state) => state?.posts);
+	const {postDetails} = posts
 	return (
-		<div className="flex flex-col justify-center items-center">
-			<form
-				onSubmit={formik.handleSubmit}
-				className="mt-1 flex max-w-sm m-auto"
-			>
-				<input
-					onBlur={formik.handleBlur("description")}
-					value={formik.values.description}
-					onChange={formik.handleChange("description")}
-					type="text"
-					name="text"
-					id="text"
-					className="shadow-sm focus:ring-indigo-500  mr-2 focus:border-indigo-500 block w-full p-2 border-1 sm:text-sm border-gray-300 rounded-md"
-					placeholder="Add New comment"
-				/>
+		<div>
+			{postDetails?.author?.isBlocked ? (
+				<h2 className="text-xl text-center text-red-500 mt-5 ">{`${postDetails?.author?.firstName} ${postDetails?.author?.lastName} is blocked`}</h2>
+			) : (
+				<div className="flex flex-col justify-center items-center">
+					<form
+						onSubmit={formik.handleSubmit}
+						className="mt-1 flex max-w-sm m-auto"
+					>
+						<input
+							onBlur={formik.handleBlur("description")}
+							value={formik.values.description}
+							onChange={formik.handleChange("description")}
+							type="text"
+							name="text"
+							id="text"
+							className="shadow-sm focus:ring-indigo-500  mr-2 focus:border-indigo-500 block w-full p-2 border-1 sm:text-sm border-gray-300 rounded-md"
+							placeholder="Add New comment"
+						/>
 
-				<button
-					type="submit"
-					className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-				>
-					Submit
-				</button>
-			</form>
-			<div className="text-red-400 mb-2 mt-2">
-				{formik.touched.description && formik.errors.description}
-			</div>
+						<button
+							type="submit"
+							className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+						>
+							Submit
+						</button>
+					</form>
+					<div className="text-red-400 mb-2 mt-2">
+						{formik.touched.description && formik.errors.description}
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };

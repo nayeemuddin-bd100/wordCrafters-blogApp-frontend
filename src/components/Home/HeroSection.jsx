d2f19d49f3a4c4d45e544acdfb8c520980418c61
Nayeem Uddin <nayeemuddin.bd100@gmail.com>
2023-11-27 11:47:39 +0600

    fix: link to feed

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -11,15 +11,15 @@ const HeroSection = () => {
 							<span className="text-lg font-bold text-blue-400">
 								Create posts to educate
 							</span>
-							<h2 className="max-w-2xl mt-12 mb-12 text-6xl 2xl:text-8xl text-white font-bold font-heading">
+							<h2 className="max-w-2xl mt-8 mb-12 text-6xl 2xl:text-8xl text-white font-bold font-heading">
 								Pen down your ideas{" "}
 								<span className="text-yellow-500">By creating a post</span>
 							</h2>
 							<Link
-								className="inline-block px-12 py-3 text-lg text-white font-bold bg-blue-500 hover:bg-blue-600 hover:text-gray-800 rounded-2xl transition duration-200"
-								to="/"
+								className="inline-block px-12 py-3 text-lg text-white font-bold bg-blue-500 hover:bg-blue-600 hover:text-gray-300 rounded-2xl transition duration-200"
+								to="/posts"
 							>
-								Buy This Course
+								Explore the feed
 							</Link>
 						</div>
 						<div className="w-full lg:w-1/2 px-4">

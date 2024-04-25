fe83cf982b8245fbaf96f10f2a7f5cb9abc3101e
nayeem <nayeemuddin.bd100@gmail.com>
2024-04-25 18:56:49 +0600

    feat: change bg color

--- src/components/Home/HeroSection.jsx
+++ src/components/Home/HeroSection.jsx
@@ -5,9 +5,9 @@ import poster from "../../img/poster.png";
 const HeroSection = () => {
   return (
     <>
-      <section className=" bg-gray-800">
-        <div className="relative container px-4 mx-auto max-w-7xl">
-          <div className="flex flex-wrap items-center -mx-4 mb-10 2xl:mb-14 pt-20 lg:pt-9 xl:pt-5 ">
+      <section className=" bg-sky-200">
+        <div className="relative container px-4 mx-auto max-w-7xl ">
+          <div className="flex flex-wrap items-center -mx-4 mb-10 2xl:mb-14 pt-20 lg:pt-9 xl:pt-5 h-[100vh] ">
             <motion.div
               initial={{ x: -200, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
@@ -15,12 +15,12 @@ const HeroSection = () => {
               className="w-full lg:w-1/2 px-4 mb-5 lg:mb-0  flex flex-col items-center lg:block"
             >
              
-              <h2 className="max-w-2xl mt-8 mb-12 text-6xl  text-white font-bold font-heading  lg:text-left text-center ">
+              <h2 className="max-w-2xl mt-8 mb-12 text-6xl  text-black font-bold font-heading  lg:text-left text-center ">
                 Pen down your ideas{" "}
-                <span className="text-yellow-500">By creating a post</span>
+                <span className="text-purple-800">By creating a post</span>
               </h2>
               <Link
-                className="inline-block px-12 py-3 text-lg text-white font-bold bg-blue-500 hover:bg-blue-600 hover:text-gray-300 rounded-lg transition duration-200"
+                className="inline-block px-12 py-3 text-lg text-white font-bold bg-pink-500 hover:bg-pink-700 hover:text-gray-300 rounded-lg transition duration-200"
                 to="/posts"
               >
                 Explore the feed

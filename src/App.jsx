f9cae96c0ecac99f3c01f95cc42201db93772eca
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-06 22:32:34 +0600

    feat: add author page

--- src/App.jsx
+++ src/App.jsx
@@ -3,65 +3,69 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddNewCategory from "./components/Category/AddNewCategory";
 import CategoryList from "./components/Category/CategoryList";
 import UpdateCategory from "./components/Category/UpdateCategory";
-import UpdateComment from './components/Comments/UpdateComment';
+import UpdateComment from "./components/Comments/UpdateComment";
 import Navbar from "./components/Home/Navigation/Navbar";
-import AdminRoute from './components/Home/Navigation/ProtectedRoute/AdminRoute';
+import AdminRoute from "./components/Home/Navigation/ProtectedRoute/AdminRoute";
 import ProtectedRoute from "./components/Home/Navigation/ProtectedRoute/ProtectedRoute";
-import CreatePost from './components/Posts/CreatePost';
-import PostDetails from './components/Posts/PostDetails';
+import CreatePost from "./components/Posts/CreatePost";
+import PostDetails from "./components/Posts/PostDetails";
 import PostsList from "./components/Posts/PostsList";
-import UpdatePost from './components/Posts/UpdatePost';
-import AccountVerified from './components/Users/AccountVerified';
-import Profile from './components/Users/Profile';
-import ResetPassword from './components/Users/ResetPassword';
-import ResetPasswordForm from './components/Users/ResetPasswordForm';
-import SendEmail from './components/Users/SendEmail';
-import UpdatePassword from './components/Users/UpdatePassword';
-import UpdateProfileForm from './components/Users/UpdateProfileForm';
-import UsersList from './components/UsersList/UsersList';
+import UpdatePost from "./components/Posts/UpdatePost";
+import AccountVerified from "./components/Users/AccountVerified";
+import Profile from "./components/Users/Profile";
+import ResetPassword from "./components/Users/ResetPassword";
+import ResetPasswordForm from "./components/Users/ResetPasswordForm";
+import SendEmail from "./components/Users/SendEmail";
+import UpdatePassword from "./components/Users/UpdatePassword";
+import UpdateProfileForm from "./components/Users/UpdateProfileForm";
+import UsersList from "./components/UsersList/UsersList";
+import About from "./pages/About";
+import Author from "./pages/Author";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Register from "./pages/Register";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Toaster />
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/password-reset-token" element={<ResetPasswordForm />} />
-				<Route path="/set-new-pass/:token" element={<ResetPassword />} />
-				<Route path="/posts" element={<PostsList />} />
-				<Route path="/posts/:id" element={<PostDetails />} />
+  return (
+    <BrowserRouter>
+      <Toaster />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/authors" element={<Author />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/password-reset-token" element={<ResetPasswordForm />} />
+        <Route path="/set-new-pass/:token" element={<ResetPassword />} />
+        <Route path="/posts" element={<PostsList />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
 
-				<Route element={<ProtectedRoute />}>
-					<Route path="/users" element={<UsersList />} />
-					<Route path="/update-password" element={<UpdatePassword />} />
-					<Route path="/create-post" element={<CreatePost />} />
-					<Route path="/update-post/:id" element={<UpdatePost />} />
-					<Route path="/update-comment/:id" element={<UpdateComment />} />
-					{/* user profile */}
-					<Route path="/profile/:id" element={<Profile />} />
-					<Route path="/update-profile/:id" element={<UpdateProfileForm />} />
-					<Route path="/send-email" element={<SendEmail />} />
-					<Route path="/verify-token/:token" element={<AccountVerified />} />
-				</Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/users" element={<UsersList />} />
+          <Route path="/update-password" element={<UpdatePassword />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<UpdatePost />} />
+          <Route path="/update-comment/:id" element={<UpdateComment />} />
+          {/* user profile */}
+          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfileForm />} />
+          <Route path="/send-email" element={<SendEmail />} />
+          <Route path="/verify-token/:token" element={<AccountVerified />} />
+        </Route>
 
-				<Route element={<AdminRoute />}>
-					<Route path="/add-category" element={<AddNewCategory />} />
-					<Route path="/category-list" element={<CategoryList />} />
-					<Route path="/update-category/:id" element={<UpdateCategory />} />
-				</Route>
+        <Route element={<AdminRoute />}>
+          <Route path="/add-category" element={<AddNewCategory />} />
+          <Route path="/category-list" element={<CategoryList />} />
+          <Route path="/update-category/:id" element={<UpdateCategory />} />
+        </Route>
 
-				<Route path="*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Author.jsx
@@ -0,0 +1,115 @@
+import { motion } from "framer-motion";
+import AuthorCard from "../components/Author/AuthorCard";
+import Footer from "../components/Footer/Footer";
+import SmoothScroll from "../hooks/smoothScroll";
+
+const Author = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <SmoothScroll>
+        <div className="max-w-7xl mx-auto my-20 px-5">
+          {/* Author card */}
+          <motion.div
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-30"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={{
+              visible: {
+                transition: {
+                  staggerChildren: 0.2,
+                },
+              },
+              hidden: {},
+            }}
+          >
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>{" "}
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>{" "}
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>{" "}
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>{" "}
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>{" "}
+            <motion.div
+              variants={{
+                visible: { opacity: 1, scale: 1 },
+                hidden: { opacity: 0, scale: 0.8 },
+              }}
+              transition={{ duration: 0.5 }}
+            >
+              <AuthorCard />
+            </motion.div>
+            {/* Repeat for additional AuthorCard components */}
+          </motion.div>
+
+          {/* Become an author */}
+
+          <div className="flex flex-col md:flex-row justify-between items-center gap-4 my-32 md:px-15 lg:px-32 ">
+            <p className="text-center md:text-left text-4xl font-bold md:w-2/3">
+              Become <span className="text-indigo-500 italic">An Author</span>{" "}
+              And Share Your Great Stories.
+            </p>
+            <div className="md:w-1/3">
+              <button className="bg-indigo-500 text-white px-4 py-2 rounded hover:bg-indigo-600 duration-700 text-xl ">
+                Become An Author
+              </button>
+            </div>
+          </div>
+        </div>
+      </SmoothScroll>
+
+      {/* Footer */}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Author;

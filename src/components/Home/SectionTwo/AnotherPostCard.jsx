339a1d324349992ac323c4a335e49ecd7cde95c6
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-06 22:34:58 +0600

    feat: add framer

--- src/components/Home/SectionTwo/AnotherPostCard.jsx
+++ src/components/Home/SectionTwo/AnotherPostCard.jsx
@@ -1,13 +1,20 @@
+import { Link } from 'react-router-dom';
+
 const AnotherPostCard = () => {
+
   return (
-    <div className="flex items-center content-center gap-5 lg:gap-2 justify-start my-5">
-      <img
-        className="w-1/3 rounded-lg object-cover h-20"
-        src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60"
-        alt="another post image"
-      />
+    <div className="flex items-center content-center gap-5 lg:gap-2 justify-start my-5 group">
+   
+     <div className="relative w-1/3 h-20 overflow-hidden rounded-lg">
+        <img
+          className="object-cover w-full h-full group-hover:scale-125 transition-all duration-500"
+          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60"
+          alt="another post image"
+        />
+      </div>
+
       <div className="flex flex-col gap-1.5">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        <Link className="hover:text-indigo-700 transition-all duration-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
 
         <p className="text-xs text-gray-800 font-inter flex items-center gap-3">
           Nov 10 2022
--- src/components/Home/SectionTwo/RecentPostCard.jsx
+++ src/components/Home/SectionTwo/RecentPostCard.jsx
@@ -3,26 +3,29 @@ import { Link } from "react-router-dom";
 
 const RecentPostCard = () => {
   return (
-    <div className="overflow-hidden font-prompt">
+    <div className="font-prompt group">
+
+      <div className="relative overflow-hidden rounded-t-xl">
       <img
-        className="rounded-t-xl object-cover"
+        className="rounded-t-xl object-cover group-hover:scale-105 transition-all duration-500 "
         src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60"
         alt="img"
       />
+      </div>
 
-      <div>
+      <div className="mt-5">
         <p className="text-xs text-gray-800 font-inter flex items-center gap-3">
           By Nayeem Uddin{" "}
           <span className="w-2 h-2 rounded-full bg-indigo-300 inline-block"></span>{" "}
           <span> TagName</span>
         </p>
-        <p className="text-lg font-medium">
+        <Link className="text-lg font-medium hover:text-indigo-700 transition-all duration-300">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat,
           reiciendis.
-        </p>
+        </Link>
 
         <div className="flex justify-between mt-3">
-          <Link className="text-sm text-indigo-800">
+          <Link className="text-sm text-indigo-800 hover:text-indigo-400 transition-all duration-500">
             View Details{" "}
             <span>
               <ArrowRightIcon className="w-4 h-4 inline-block" />

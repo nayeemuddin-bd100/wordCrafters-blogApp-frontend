c26deba7566bed365e607ee8c83e3ec374b9935e
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-09 03:07:21 +0600

    feat: add postlist data

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AdminRoute from "./components/Home/Navigation/ProtectedRoute/AdminRoute";
 import ProtectedRoute from "./components/Home/Navigation/ProtectedRoute/ProtectedRoute";
 import CreatePost from "./components/Posts/CreatePost";
 import PostDetails from "./components/Posts/PostDetails";
+import PostsList from './components/Posts/PostsList';
 import UpdatePost from "./components/Posts/UpdatePost";
 import AccountVerified from "./components/Users/AccountVerified";
 import Profile from "./components/Users/Profile";
@@ -17,7 +18,6 @@ import ResetPasswordForm from "./components/Users/ResetPasswordForm";
 import SendEmail from "./components/Users/SendEmail";
 import UpdatePassword from "./components/Users/UpdatePassword";
 import UpdateProfileForm from "./components/Users/UpdateProfileForm";
-import UsersList from "./components/UsersList/UsersList";
 import About from "./pages/About";
 import Author from "./pages/Author";
 import Blog from './pages/Blog';
@@ -41,19 +41,20 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/password-reset-token" element={<ResetPasswordForm />} />
         <Route path="/set-new-pass/:token" element={<ResetPassword />} />
-        {/* <Route path="/posts" element={<PostsList />} /> */}
+        <Route path="/posts" element={<PostsList />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/blog/:id" element={<BlogDetails />} />
         <Route path="/posts/:id" element={<PostDetails />} />
+        {/* <Route path="/author/:id" element={<Profile />} /> */}
 
         <Route element={<ProtectedRoute />}>
-          <Route path="/users" element={<UsersList />} />
+          {/* <Route path="/users" element={<UsersList />} /> */}
           <Route path="/update-password" element={<UpdatePassword />} />
           <Route path="/create-post" element={<CreatePost />} />
           <Route path="/update-post/:id" element={<UpdatePost />} />
           <Route path="/update-comment/:id" element={<UpdateComment />} />
           {/* user profile */}
-          <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/author/:id" element={<Profile />} />
           <Route path="/update-profile/:id" element={<UpdateProfileForm />} />
           <Route path="/send-email" element={<SendEmail />} />
           <Route path="/verify-token/:token" element={<AccountVerified />} />

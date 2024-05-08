1448c418287161b9f04ffc7a53f78927d5224a41
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-09 03:06:53 +0600

    feat: complete author page

--- src/components/Author/AuthorCard.jsx
+++ src/components/Author/AuthorCard.jsx
@@ -1,31 +1,95 @@
-import { motion } from 'framer-motion';
-import { Link } from "react-router-dom";
-import Author1 from "../../img/author/author1.jpg";
+/* eslint-disable react/prop-types */
+import { motion } from "framer-motion";
+import { toast } from "react-hot-toast";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const AuthorCard = () => {
+import { deleteUserAction } from "../../redux/slices/users/usersSlices";
+
+const AuthorCard = ({ author }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loggedInUser = useSelector((state) => state?.users?.profile);
+
+  const handleDelete = (id) => {
+    const shouldDelete = window.confirm(
+      "Are you sure you want to delete this user?"
+    );
+    if (shouldDelete) {
+      if (!loggedInUser?.isAdmin) {
+        return toast.error("Only Admin can delete author");
+      } else if (author.email === "nayeemuddin.bd100@gmail.com") {
+        return toast.error("Demo Admin cannot delete Super Admin");
+      } else if (author.email === "wordcrafters@admin.com") {
+        return toast.error("Demo Admin cannot delete Own Account");
+      } else if (author.email === "wordcrafters@user.com") {
+        return toast.error("Demo Admin cannot delete Demo User Account");
+      }
+
+      dispatch(deleteUserAction(id));
+      toast.success("User deleted successfully");
+    }
+  };
+
+   const handleViewProfile = () => {
+    if (!loggedInUser?.id) {
+      toast.error("Please login first to view author info");
+    } else {
+      navigate(`/author/${author?._id}`);
+    }
+  };
+
   return (
     <motion.div
       animate={{ opacity: 1, scale: 1 }}
       initial={{ opacity: 0, scale: 0.5 }}
       transition={{ duration: 1 }}
     >
-      <div className="flex flex-col justify-center items-center gap-1 rounded-xl border-gray-800 bg-rose-100 py-4 mx-10 sm:mx-0 group group-hover:bg-rose-200">
+      <div className="flex flex-col justify-center items-center gap-1 rounded-xl border-gray-800 bg-rose-100 py-4  group group-hover:bg-rose-200">
         <div className="">
           <img
-            src={Author1}
+            src={author?.profilePhoto}
             alt="author"
             className=" w-40 h-40 rounded-full object-cover group-hover:scale-105 transition-all duration-500 "
           />
         </div>
         <div className="flex flex-col items-center justify-center gap-2 ">
-          <p className="text-xl font-semibold ">Lionel Messi</p>
-          <p className="font-inter text-sm text-gray-900 font-medium ">
-            5 Article
+          <p className="text-xl font-semibold ">
+            {author?.firstName} {author?.lastName}
           </p>
-          <Link className=" bg-indigo-500 hover:bg-indigo-400 transition-all duration-500 text-white  px-3 py-1 rounded-lg">
+
+          <p className=" text-purple-500 font-inter font-semibold">
+            {author?.email === "nayeemuddin.bd100@gmail.com"
+              ? "Super Admin"
+              : author?.email === "wordcrafters@admin.com"
+              ? "Demo Admin"
+              : author?.email === "wordcrafters@user.com"
+              ? "Demo User"
+              : author?.accountType}
+          </p>
+
+          <p className="font-inter text-sm text-gray-900 font-medium ">
+            {author?.posts?.length} Article
+          </p>
+          <button
+            // to={`/author/${author?._id}`}
+            onClick={() => handleViewProfile()}
+            className=" bg-indigo-500 hover:bg-indigo-400 transition-all duration-500 text-white  px-3 py-1 rounded-lg"
+          >
             {" "}
             View Profile{" "}
-          </Link>
+          </button>
+
+          {loggedInUser?.id && (
+            <button
+              onClick={() => handleDelete(author?._id)}
+              className=" bg-red-500 hover:bg-red-400 transition-all duration-500 text-white  px-3 py-1 rounded-lg"
+            >
+              {" "}
+              Delete
+            </button>
+          )}
         </div>
       </div>
     </motion.div>

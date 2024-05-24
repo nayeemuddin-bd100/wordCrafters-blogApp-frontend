946343450ea6ab0bf76b78ddd6d361efca9e8cb7
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-24 12:26:07 +0600

    feat: complete another post card

--- src/components/Home/SectionTwo/AnotherPostCard.jsx
+++ src/components/Home/SectionTwo/AnotherPostCard.jsx
@@ -1,25 +1,36 @@
-import { Link } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+import TextTruncate from "react-text-truncate";
+import dateFormatter from "../../../utils/dateFormatter";
 
-const AnotherPostCard = () => {
+const AnotherPostCard = ({ post }) => {
+  const { _id, title, image, createdAt, comments } = post;
 
   return (
-    <div className="flex items-center content-center gap-5 lg:gap-2 justify-start my-2 group w-full">
-   
-     <div className="relative w-1/3 h-20 overflow-hidden rounded-lg">
+    <div className="flex items-center gap-5 lg:gap-2 justify-start my-2 group w-full">
+      <div className="relative w-1/3 h-20 overflow-hidden rounded-lg">
         <img
-          className="object-cover w-48 h-32 group-hover:scale-105 transition-all duration-500"
-          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60"
+          className=" object-cover w-full  h-32 group-hover:scale-105 transition-all duration-500"
+          src={image}
           alt="another post image"
         />
       </div>
 
-      <div className="flex flex-col gap-1.5">
-        <Link className="hover:text-red-500 transition-all duration-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</Link>
+      <div className="flex flex-col w-2/3 gap-1.5">
+        <Link className="hover:text-red-500 transition-all duration-300">
+          {" "}
+          <TextTruncate
+            line={3}
+            element="span"
+            truncateText="..."
+            text={title}
+          />
+        </Link>
 
         <p className="text-xs text-gray-800 font-inter flex items-center gap-3">
-          Nov 10 2022
+          {dateFormatter(createdAt)}
           <span className="w-2 h-2 rounded-full bg-indigo-300 inline-block"></span>{" "}
-          <span> 5 comment</span>
+          <span> {comments?.length} comment</span>
         </p>
       </div>
     </div>

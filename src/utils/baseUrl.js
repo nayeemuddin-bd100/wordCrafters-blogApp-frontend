94f81a440aa1e40f87500ec30f76a0e16c6a7849
nayeem <nayeemuddin.bd100@gmail.com>
2024-05-27 22:00:06 +0600

    feat: update production url

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,5 @@
 // const baseUrl = "http://localhost:5000"
-const baseUrl = "https://wordcrafters.onrender.com"
+// const baseUrl = "https://wordcrafters.onrender.com"
+const baseUrl = "https://wordcrafters-blog-backend.vercel.app/"
 
 export default baseUrl
